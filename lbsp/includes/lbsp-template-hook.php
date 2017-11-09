4ba301164310d8cf748c4607e588bf3ca621d9ac
pinzest <shivbhargav.official@gmail.com>
2017-11-09 09:44:53 +0530

    edit

--- lbsp/includes/lbsp-template-hook.php
+++ lbsp/includes/lbsp-template-hook.php
@@ -18,18 +18,6 @@
 add_action( 'lbsp_before_single_book_summary', 'lbsp_show_book_images', 20 );
 
 /**
- * Product Summary Box.
- *
- * @see woocommerce_template_single_title()
- * @see woocommerce_template_single_rating()
- * @see woocommerce_template_single_price()
- * @see woocommerce_template_single_excerpt()
- * @see woocommerce_template_single_meta()
- * @see woocommerce_template_single_sharing()
+ * Book Summary Box.
  */
 add_action( 'lbsp_single_book_summary', 'lbsp_template_single_title', 5 );
-/*add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );*/
